a32626bae32dc52d7f164897c9cb569bd501bf58
ASHFAQ AFZAL CHOWDHURY <aacfahim@gmail.com>
2020-07-23 17:29:07 +0600

    Syntax fix

--- PHP_TASK_03/Assignment.php
+++ PHP_TASK_03/Assignment.php
@@ -24,7 +24,7 @@ function EmailCheck()
 	{
 		$sEmail = $_POST['Email'];
 
-		if($sEmail == "" || checkemailPattern($sEmail))
+		if($sEmail == "" || !checkemailPattern($sEmail))
 		{
 			echo "Valid Email is required";
 		}
@@ -52,142 +52,173 @@ function DOB()
 {
 	if($_SERVER["REQUEST_METHOD"] == "POST")
 	{
-		$s = $_POST['gender'];
+		$sDOB = $_POST['dob'];
 
-		if($sGender == "")
+		if($sDOB == "")
 		{
-			echo "Please select your gender";
+			echo "Date of birth required";
 		}
 	}
 
 }
 
+function BG()
+{
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		$bloodGroup = $_POST['blood'];
+
+		if(empty($bloodGroup))
+		{
+			echo "Blood Group is required";
+		}
+	}
+
+}
+
+
+function CheckDegree()
+{
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		//$Sdegree = $_POST['check'];
+
+		if(empty($_POST['check']))
+		{
+			echo "check the degrees";
+		}
+	}
+
+}
+
+function CheckFile()
+{
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		//$Sdegree = $_POST['check'];
+
+		if(empty($_POST['photo']))
+		{
+			echo "Attach your photo";
+		}
+	}
+
+}
+
+
+
+
 ?>
 
 
 <html>
+<head>
+	<title>Person Profile</title>
+</head>
 <body>
 <form method="POST">
-<table width="50%" align="center">
-<tr>
-<td colspan="2">
- <center><h1>Person Profile</h1></center>
-  </td>
+	<table width="100%" align="center">
+		<tr>
+			<td colspan="2">
+			<center><h1>Person Profile</h1></center>
+			</td>
+		</tr>
+		<tr>
+			<td>
 
-</tr>
-<tr>
+			<b>Name:</b>
+			</td>
+			<td>
+			<input type="text" name="name" value=""><?php nameCheck(); ?> <br/>
+			</td>
+		</tr>
 
-<td>
+		<tr>
+			<td>
+			<b>Email: </b>
+			</td>
+			<td>
+			<input type="Email" name="Email"><?php EmailCheck(); ?> <br/>
+			</td>
+		</tr>
 
-<b>Name:</b>
-</td>
+		<tr>
+			<td>
+			<b>Gender: </b>
+			</td>
 
-<td>
-<input type="text" name="name" value=""><?php nameCheck(); ?> <br/>
-	</td>
+			<td>
+			<input type="radio" id="male" name="gender" value="male">
+			<label for="male"><b>Male</b></label>
+			<input type="radio" id="female" name="gender" value="female">
+			<label for="female"><b>Female</b></label>
+			<input type="radio" id="other" name="gender" value="other">
+			<label for="other"><b>Other</b></label>  
+			
+			</td>
 
-	</tr>
-<tr>
+		</tr>
 
-<td>
-<b>Email: </b>
+		<tr>
 
-</td>
-<td>
-<input type="Email" name="Email"><?php EmailCheck(); ?> <br/>
-</td>
-</tr>
+			<td><b>Date Of Birth:</b></td>
+			<td> <input type="text" id = "day" name="dob" size="2">/<input type="text" id = "month" name="dob" size="2">/
+			<input type="text" id = "year" name="dob" size="2">
+			(dd/mm/yyyy) <?php DOB(); ?></td>
 
-<tr>
-<td>
-<b>Gender:</b>
+		</tr>
 
-</td>
-<td>
-
-<input type="radio" id="male" name="gender" value="male">
-<label for="male"><b>Male</b></label>
-<input type="radio" id="female" name="gender" value="female">
-<label for="female"><b>Female</b></label>
-<input type="radio" id="other" name="gender" value="other">
-<label for="other"><b>Other</b></label>   <?php IsGenderSelected(); ?> 
-	</td>
-
-	</tr>
-
-<tr>
-
-<td><b>Date Of Birth:</b></td>
-<td>  <input type="date" id="male" name="gender" value="male"> (dd/mm/yyyy)
-   </td>
-
-
-</tr>
-
-<tr>
-<td>
-<b>Blood Group:</b>
-</td>
-
-<td>
-<select>
-<option>A+</option>
-<option>B+</option>
-<option>AB+</option>
-<option>O+</option>
-
-
-</select>
-
-	</td>
-
-
-</tr>
-<tr>
-<td>
-
-<b>Degree:</b>
-
-</td>
-<td>
-<input type="checkbox" id="checkbox" name="check" value="check"> <b>SSC</b>
-<input type="checkbox" id="checkbox" name="check" value="check"> <b>HSC</b>
-<input type="checkbox" id="checkbox" name="check" value="check"> <b>BSc.</b>
-<input type="checkbox" id="checkbox" name="check" value="check"> <b>MSc.</b>
-</td>
-</tr>
-<tr>
-<td colspan="2">
-
-</td>
-
-
-</tr>
-
-
-<tr>
-        <td width="30%"><b>Photo</b></td>
-        <td width="70%" colspan="2">
-            <input type="file" name = "photo" >
+		<tr>
+		<td>
+		<b>Blood Group:</b>
 		</td>
-</tr>
-	
 
-<tr>
+		<td>
+			<select name="blood">
+				<option></option>
+				<option >A+</option>
+				<option >B+</option>
+				<option>AB+</option>
+				<option >O+</option>
+			</select>
+			<?php BG(); ?>
+			</td>
+		</tr>
 
-<td colspan="2">
-	
-    <input type="submit" value="Submit">
-<input type="button" value="Reset">
-</td>
+	<tr>
+		<td>
+		<b>Degree:</b>
+		</td>
 
-</tr>
+		<td>
+		<input type="checkbox" id="checkbox" name="check" value=""> <b>SSC</b>
+		<input type="checkbox" id="checkbox" name="check" value=""> <b>HSC</b>
+		<input type="checkbox" id="checkbox" name="check" value=""> <b>BSc.</b>
+		<input type="checkbox" id="checkbox" name="check" value=""> <b>MSc.</b>
+		<? CheckDegree(); ?>
+		</td>
+	</tr>
+
+	<tr>
+		<td width="30%"><b>Photo</b></td>
+		<td width="70%" colspan="2">
+		<input type="file" name = "photo" >
+		<?php CheckFile(); ?>
+		</td>
+	</tr>
+		
+
+	<tr>
+
+		<td colspan="2" align="center"><br>
+		<input type="submit" value="Submit" >
+		<input type="button" value="Reset">
+		</td>
+	</tr>
 
 
-</table>
+	</table>
 
 </form>
 </body>
-
-
 </html>

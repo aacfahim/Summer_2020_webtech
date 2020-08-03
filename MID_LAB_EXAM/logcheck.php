189413d4325f93cf2d29d29718009961636b2c1b
ASHFAQ AFZAL CHOWDHURY <aacfahim@gmail.com>
2020-08-03 12:08:04 +0600

    admin user home added

--- MID_LAB_EXAM/logcheck.php
+++ MID_LAB_EXAM/logcheck.php
@@ -12,10 +12,36 @@
         }
         else{
             if(isset($_COOKIE['userName']) && isset($_COOKIE['password'])){
+
                 if($userName == $_COOKIE['userName'] && $password == $_COOKIE['password']){
 
                     setcookie('STATUS', 'OK', time()+3600, '/');
                     header('location: dashboard.php');
+
+                    $file  = fopen('user.txt', 'r');
+			        $data  = fread($file, filesize('user.txt'));
+                    fclose($data);
+                    
+                    $Adminfile  = fopen('admin.txt', 'r');
+			        $Admindata  = fread($Adminfile, filesize('admin.txt'));
+			        fclose($Admindata);
+
+                    $user = explode('|', $data);
+                    $admin = explode('|', $Admindata);
+
+                    if($userName == trim($user[0]) && $password == trim($user[1])){
+				        $_SESSION['status'] = "OK";
+				        header('location: UserHome.html');
+                    }
+                    else if($userName == trim($admin[0]) && $password == trim($admin[1]))
+                    {
+                        $_SESSION['status'] = "OK";
+                        header('location: AdminHome.html');
+
+                    }
+                else{
+				    header('location: login.php?msg=invalid_username/password');
+			    }
 
 
                 }else{
